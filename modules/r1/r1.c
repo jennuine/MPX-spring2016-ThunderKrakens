25ab4d23d43f42d08d2977e44ffea4f16845ca6d
Jenn <jqnguyen@mix.wvu.edu>
2016-02-01 23:40:17 -0500

    added version function & modified shutdown procedure; both working correctly

--- modules/r1/r1.c
+++ modules/r1/r1.c
@@ -7,7 +7,11 @@
 
 #define USER_INPUT_BUFFER_SIZE 1000
 #define MAX_ARGC 50
-#define NUM_OF_FUNCTIONS 2
+#define NUM_OF_FUNCTIONS 3
+#define NUM_OF_MPX_FUNCTIONS 2
+#define VERSION "R1"
+
+static int run_mpx = 1;
 
 typedef struct
 {
@@ -15,11 +19,16 @@ typedef struct
 	int (*function) (int argc, char ** argv);
 } function_name;
 
+static int version();
+static int shutdown();
+static int mpx_function();
+
 static int exe_function(int argc, char ** argv)
 {//This is a list of functions that associate with the name of them.
-	static function_name functionList[NUM_OF_FUNCTIONS];
+	static function_name functionList[NUM_OF_MPX_FUNCTIONS];
 	functionList[0].nameStr = "gettime"; functionList[0].function = &get_time_main;
 	functionList[1].nameStr = "settime"; functionList[1].function = &set_time_main;
+	functionList[2].nameStr = "mpx"; functionList[2].function = &mpx_function;
 	//Note: change the value in the #define NUM_OF_FUNCTIONS before adding more at here!
 
 	int i = 0;
@@ -31,41 +40,58 @@ static int exe_function(int argc, char ** argv)
 		}
 	}
 
-	printf("There is no function called %s. Please refers to \"help\"\n", argv[0]);
+	printf("There is no function called %s. Please refer to \"help\"\n", argv[0]);
 	return 0;
 }
 
-static int shutdown(const char* cmd_str)
+static int mpx_function(int argc, char ** argv)
 {
-	static const int SHUTDOWN_RETURN = 1;
-	if (strcmp(cmd_str, "mpx-shutdown") != 0)
-		return !SHUTDOWN_RETURN;
-	else
+	static function_name mpx_functions[NUM_OF_FUNCTIONS];
+	mpx_functions[0].nameStr = "-version";	mpx_functions[0].function = &version;
+	mpx_functions[1].nameStr = "-shutdown"; mpx_functions[1].function = &shutdown;
+
+	int i = 0;
+	for (i = 0; i < NUM_OF_MPX_FUNCTIONS; i++)
 	{
-		char ans[] = { 0, 0 };
-		printf("\nAre you sure you would like to shutdown?\n( Type y/n )\n");
-
-		while (!ans[0])
-		 	if (inb(COM1 + 5) & 1)
-				ans[0] = inb(COM1);
-
-		printf("\n");
-		if (strcmp(ans, "y") == 0 || strcmp(ans, "Y") == 0)
-			return SHUTDOWN_RETURN;
-		else if (strcmp(ans, "n") == 0 || strcmp(ans, "N") == 0)
-			return !SHUTDOWN_RETURN;
-		else
-			{
-				printf("Error: %s was invalid.\n\n", ans);
-				return !SHUTDOWN_RETURN;
-			}
+		if (!strcmp(mpx_functions[i].nameStr, argv[1]))
+			return mpx_functions[i].function(argc, argv);
 	}
+
+	printf("There is no mpx function called %s. Please refer to \"help\"\n", argv[1]);
+	return 0;
+}
+
+static int version ()
+{
+		printf("\nMPX-THUNDER-KRAKENS\nVERSION\t%s\n\n", VERSION);
+		return 0;
+}
+
+static int shutdown()
+{
+
+	char ans[] = { 0, 0 };
+	printf("\nAre you sure you would like to shutdown?\n( Type y/n )\n");
+
+	while (!ans[0])
+	 	if (inb(COM1 + 5) & 1)
+			ans[0] = inb(COM1);
+
+	printf("\n");
+	if (!strcmp(ans, "y") || !strcmp(ans, "Y"))
+		return run_mpx = !run_mpx;
+	else if (!strcmp(ans, "n") || !strcmp(ans, "N"))
+		return run_mpx;
+	else
+		{
+			printf("Error: %s was invalid.\n\n", ans);
+			return run_mpx;
+		}
 }
 
 int commhand()
 {
 	static char userInput[USER_INPUT_BUFFER_SIZE];
-	static int bIsGoingShutdown = 0;
 	static int argc = 0;
 	static char ActArgArray[MAX_ARGC][USER_INPUT_BUFFER_SIZE];
 	static char * argv[MAX_ARGC];
@@ -75,23 +101,19 @@ int commhand()
 		argv[i] = ActArgArray[i];
 	}
 
-	printf("Welcome! This is Thunder Krakens Operating System!\n");
+	printf("\nWelcome! This is Thunder Krakens Operating System!\n\n");
 
-	while(!bIsGoingShutdown)
+	while(run_mpx)
 	{
 		printf("Cmd > ");
 		get_input_line(userInput, USER_INPUT_BUFFER_SIZE, WithEcho);
 		command_line_parser(userInput, &argc, argv, MAX_ARGC, USER_INPUT_BUFFER_SIZE);
-		if(argc > 0 && strcmp("mpx-shutdown", argv[0]))  //if there are some argument, except for "mpx-shutdown"
+		if(argc > 0)  //if there are some argument
 		{
 			exe_function(argc, argv);
-		}else if(argc > 0)
-		{//check if it is shutdown
-			bIsGoingShutdown = shutdown(argv[0]);
 		}
 		argc = 0;  //reset the argument list
 	}
-
 	return 0;
 }
 
