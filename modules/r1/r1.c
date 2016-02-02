8eef6621776667995c923c0aab989718d1d66020
Jenn <jqnguyen@mix.wvu.edu>
2016-02-02 12:41:12 -0500

    fixed function usage - user must type in mpx [function] to execute; made usage & help for each function; TODO: Need to correct printing out correctly

--- modules/r1/r1.c
+++ modules/r1/r1.c
@@ -7,63 +7,56 @@
 
 #define USER_INPUT_BUFFER_SIZE 1000
 #define MAX_ARGC 50
-#define NUM_OF_FUNCTIONS 3
-#define NUM_OF_MPX_FUNCTIONS 2
+#define NUM_OF_FUNCTIONS 4
 #define VERSION "R1"
-
-static int run_mpx = 1;
+#define COMPLETION "02/05/2016"
 
 typedef struct
 {
 	char * nameStr;
 	int (*function) (int argc, char ** argv);
+	char * usage;
+	char * help;
 } function_name;
+
+static int run_mpx = 1;
+static function_name functionList[NUM_OF_FUNCTIONS];
 
 static int version();
 static int shutdown();
-static int mpx_function();
 
 static int exe_function(int argc, char ** argv)
 {//This is a list of functions that associate with the name of them.
-	static function_name functionList[NUM_OF_MPX_FUNCTIONS];
-	functionList[0].nameStr = "gettime"; functionList[0].function = &get_time_main;
-	functionList[1].nameStr = "settime"; functionList[1].function = &set_time_main;
-	functionList[2].nameStr = "mpx"; functionList[2].function = &mpx_function;
+	functionList[0].nameStr = "gettime"; functionList[0].function = &get_time_main; functionList[0].usage = "mpx gettime";
+	functionList[0].help = "gettime : gettime\n\n\tDisplays the current system's time in the format HH:MM:SS.\n\n\
+		Exit Status: Always Succeeds.\n";
+	functionList[1].nameStr = "settime"; functionList[1].function = &set_time_main; functionList[1].usage = "mpx settime [HH:MM:SS]";
+	functionList[1].help = "settime : settime [HH:MM:SS]\n\n\tSets the system's time to the specified HH:MM:SS.\
+		\n\nArguments:\n\tHH\tSpecified hours ranging from 00 (midnight) to 23 (1AM.)\n\tMM\tSpecified minutes.\n\tSS\tSpecified seconds.\n\
+		Exit Status: Always Succeeds.\n";
+	functionList[2].nameStr = "-version"; functionList[2].function = &version; functionList[2].usage = "mpx -version";
+	functionList[2].help = "version : -version\n\n\tPrints the current version of MPX and completion date.\n\nExit Status: Always Succeeds.\n";
+	functionList[3].nameStr = "-shutdown"; functionList[3].function = &shutdown; functionList[3].usage = "mpx -shutdown";
+	functionList[3].help = "shutdown : -shutdown\n\n\tShuts down the operating system.\n\nExit Status: Always Succeeds.\n";
+
 	//Note: change the value in the #define NUM_OF_FUNCTIONS before adding more at here!
 
 	int i = 0;
 	for(i = 0; i < NUM_OF_FUNCTIONS; i++)
 	{
-		if(!strcmp(functionList[i].nameStr, argv[0]))
+		if(!strcmp(functionList[i].nameStr, argv[1]))
 		{
 			return functionList[i].function(argc, argv);
 		}
 	}
 
-	printf("There is no function called %s. Please refer to \"help\"\n", argv[0]);
-	return 0;
-}
-
-static int mpx_function(int argc, char ** argv)
-{
-	static function_name mpx_functions[NUM_OF_FUNCTIONS];
-	mpx_functions[0].nameStr = "-version";	mpx_functions[0].function = &version;
-	mpx_functions[1].nameStr = "-shutdown"; mpx_functions[1].function = &shutdown;
-
-	int i = 0;
-	for (i = 0; i < NUM_OF_MPX_FUNCTIONS; i++)
-	{
-		if (!strcmp(mpx_functions[i].nameStr, argv[1]))
-			return mpx_functions[i].function(argc, argv);
-	}
-
-	printf("There is no mpx function called %s. Please refer to \"help\"\n", argv[1]);
+	printf("There is no function called %s. Please refer to \"help\"\n", argv[1]);
 	return 0;
 }
 
 static int version ()
 {
-		printf("\nMPX-THUNDER-KRAKENS\nVERSION\t%s\n\n", VERSION);
+		printf("\nMPX-THUNDER-KRAKENS\nCOMPLETION\t%s\nVERSION\t%s\n\n", COMPLETION, VERSION);
 		return 0;
 }
 
@@ -89,6 +82,18 @@ static int shutdown()
 		}
 }
 
+static void list_functions()
+{
+	printf("\nAvailable mpx functions:\n");
+
+	int i;
+	for (i = 0; i < NUM_OF_FUNCTIONS; i++)
+	{
+		printf("\tusage: %s\n", functionList[i].usage);
+	}
+	printf("\n");
+}
+
 int commhand()
 {
 	static char userInput[USER_INPUT_BUFFER_SIZE];
@@ -108,9 +113,16 @@ int commhand()
 		printf("Cmd > ");
 		get_input_line(userInput, USER_INPUT_BUFFER_SIZE, WithEcho);
 		command_line_parser(userInput, &argc, argv, MAX_ARGC, USER_INPUT_BUFFER_SIZE);
-		if(argc > 0)  //if there are some argument
+		if(!strcmp(argv[0], "mpx"))
 		{
-			exe_function(argc, argv);
+			if (argc > 1)
+				exe_function(argc, argv);
+			else
+				list_functions();
+		}
+		else
+		{
+			printf("There is no program called \"%s\".\n", argv[0]);
 		}
 		argc = 0;  //reset the argument list
 	}
