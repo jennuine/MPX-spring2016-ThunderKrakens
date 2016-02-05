49bf4e82c504450c6778e1c537312387bc2ddc8d
Jafar Alkusaimi <jtalkusaimi@mix.wvu.edu>
2016-02-05 02:47:45 +0000

    sys_clock.h - Documentation

--- modules/r1/sys_clock.h
+++ modules/r1/sys_clock.h
@@ -1,54 +1,102 @@
+/**
+ * @brief System Clock and Date Header
+ * @file sys_clock.h
+ * @author Thunder Krakens
+ * @date February 2nd, 2016
+ *
+ * The header of the main file that manipulates and controls the system's clock.
+ *
+ */
+ 
 #ifndef _SYS_CLOCK_H
 #define _SYS_CLOCK_H
 
 #include <system.h>
 #include "../errno.h"
 
-/*
-  Procedure..: set_time_main
-  Description..: The main function for set time.
-*/
+/** @brief  Name: set_time_main.
+ *
+ * Description: The main fuction of time set for the system.
+ * @param argc - The number of tokens found.
+ * @param argv - The array of tokens. 
+ */
+ 
 int set_time_main(int argc, char** argv);
 
-/*
-  Procedure..: get_time_main
-  Description..: The main function for get time.
-*/
+/** @brief  Name: get_time_main.
+ *
+ * Description: Retrieves system's current time.
+ * @param argc - The number of tokens found.
+ * @param argv - The array of tokens. 
+ *
+ */
 int get_time_main(int argc, char** argv);
 
-/*
-  Procedure..: set_time_help
-  Description..: print out the help message for settime.
-*/
+/** @brief  Name: set_time_help.
+ *
+ * Description: Print out the help message for settime.
+ * @param 
+ * @return  VOID
+ */
+ 
 void set_time_help();
 
 /*
-  Procedure..: get_time_help
-  Description..: print out the help message for gettime.
-*/
+/** @brief  Name: get_time_help.
+ *
+ * Description: Print out the help message for settime.
+ * @param
+ * @return  VOID
+ */
+ 
 void get_time_help();
 
-/*
-  Procedure..: set_time_str
-  Description..: set the system time by using string.
-*/
+/** @brief  Name: set_time_str.
+ *
+ * Description: Sets the time for the system by string.
+ * @param timeStr - The string type of current Time.
+ * @return dateTimeValues - Returns the set time of the system
+ * @return E_INVSTRF - Invalid String
+ */
+ 
 error_t set_time_str(const char * timeStr);
 
-/*
-  Procedure..: get_time
-  Description..: Gets the system time.
-  Params..: dateTimeValues-output the date & time value.
-*/
+/** @brief  Name: get_time.
+ *
+ * Description: Retrieves system's current time and date.
+ * @param dateTimeValues - The value of current time and date
+ * @return VOID
+ */
+ 
 void get_time(date_time * dateTimeValues);
 
-/*
-  Procedure..: set_time
-  Description..: Sets the system time.
-  Params..: dateTimeValues-input the date & time value.
-*/
+/** @brief  Name: error_t set_time.
+ *
+ * Description: Error handling of set_time function has invalid value, or invalid format.
+ * @param dateTimeValues - The value of current time and date
+ * @return E_NOERROR - When no error was detected
+ * @return E_INVPARA - Invalid Parameter
+ */
+ 
 error_t set_time(const date_time * dateTimeValues);
 
+/** @brief  Name: set_date_main.
+ *
+ * Description: Sets system's date.
+ * @param argc - The number of tokens found.
+ * @param argv - The array of tokens. 
+ * @return  0
+ */
+
 int set_date_main(int argc, char** argv);
+
+/** @brief  Name: get_date_main.
+ *
+ * Description: Retrieves system's current date.
+ * @param argc - The number of tokens found.
+ * @param argv - The array of tokens. 
+ * @return  0
+ */
 
 int get_date_main(int argc, char** argv);
 
