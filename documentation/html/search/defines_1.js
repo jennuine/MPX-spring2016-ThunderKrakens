98536bea7aa98a98045d7acd96d1fd8221f79042
Jenn <jqnguyen@mix.wvu.edu>
2016-02-09 14:28:48 -0500

    corrected r1.c doc for doxygen; reran doxygen & made pdf

--- documentation/html/search/defines_1.js
+++ documentation/html/search/defines_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['e_5finvpara',['E_INVPARA',['../errno_8h.html#ad38311f7eadb9042f7c98dd31ffc69c3',1,'errno.h']]],
   ['e_5finvstrf',['E_INVSTRF',['../errno_8h.html#a17ad8897d139d58bb84714b2e7861ba2',1,'errno.h']]],
+  ['e_5finvusri',['E_INVUSRI',['../errno_8h.html#a5273e7742dd7c18937e5191b8070f08f',1,'errno.h']]],
   ['e_5fnoerror',['E_NOERROR',['../errno_8h.html#a910656a3ce04eecb0e4479fd35d343fb',1,'errno.h']]]
 ];
