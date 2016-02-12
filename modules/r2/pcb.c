d7fd0116d53be0970f4ddbd94d782c2f37b9ca20
Jenn <jqnguyen@mix.wvu.edu>
2016-02-11 23:36:43 -0500

    added Jafar's functions

--- modules/r2/pcb.c
+++ modules/r2/pcb.c
@@ -33,8 +33,8 @@ error_t suspend_pcb(struct pcb_struct * pcb_ptr){
 
   if (remove_pcb(pcb_ptr) != E_NOERROR){
     return E_REMOVE_PCB;
-  } 
-  
+  }
+
   return 0;
 }
 
@@ -45,7 +45,7 @@ error_t resume_pcb(struct pcb_struct * pcb_ptr){
   }
   if (remove_pcb(pcb_ptr) != E_NOERROR){
     return E_REMOVE_PCB;
-  } 
+  }
   return 0;
 }
 
@@ -148,4 +148,73 @@ struct pcb_struct * find_pcb(const char * pName)
   }
 
   return NULL;
+}
+
+/**
+ * @name insert_pcb
+ * @brief Inserts PCB into the appropriate queue.
+ *
+ * @param pcb_ptr 	The PCB pointer.
+ * @return E_INSERT_PCB	The PCB was inserted.
+ *
+ */
+
+
+error_t insert_pcb (struct pcb_struct * pcb_ptr)
+{
+    struct pcb_queue_node * current;
+
+    //double check how to check enum equals -> It is the other way around
+    if (pcb -> running_state == process_state.ready)
+    {
+        current = ready_queue.head;
+
+        while (current != ready_queue->tail)
+        {
+
+                if (current->actual_pcb->priority <= pcb->priority
+		 && pcb->priority < current->next->actual_pcb->priority)
+                {
+                    current->next = pcb->next;
+                    current->prev = pcb;
+
+                    pcb->next = current;
+                    pcb->next->prev = current;
+
+		    return E_INSERT_PCB;
+                    break;
+                }
+
+            current = current->next;
+        }
+    }
+
+	return E_INSERT_PCB_FAIL;
+}
+
+/**
+ * @name remove_pcb
+ * @brief Removes PCB from the queue it is currently in.
+ *
+ * @param pcb_ptr 	The PCB pointer.
+ * @return E_REMOVE_PCB 	PCB removed
+ * @return E_REMOVE_PCB_FAIL	Failed to remove PCB
+ *
+ */
+
+error_t remove_pcb(struct pcb_struct * pcb_ptr)
+{
+	struct pcb * current = find_pcb();
+
+	if (pcb_ptr == NULL )
+	{
+  	return E_NOT_FOUND;
+	}
+
+	current->next->prev = current->prev;
+	current->prev->next = current->next;
+	free_pcb(current);
+
+ 	return E_REMOVE_PCB;
+
 }
