709a6e2d9c6ff3d0a035a9714b1b534f5da64dfa
Jenn <jqnguyen@mix.wvu.edu>
2016-02-11 22:44:20 -0500

    added find_pcb and free_pcb

--- modules/r2/pcb.c
+++ modules/r2/pcb.c
@@ -8,7 +8,7 @@
  */
 
 #include "pcb.h"
-#include<string.h>
+#include <string.h>
 #include "../mpx_supt.h"
 
 static struct pcb_queue ready_queue;
@@ -56,4 +56,72 @@ struct pcb_struct * setup_pcb(const char * pName, const enum process_class pClas
   }
 
   return a_pcb;
+}
+
+/**
+ * @name free_pcb
+ * @brief Frees all memory associated with given PCB, including the PCB itself, the stack, etc, with sys_free_mem()
+ *
+ * @param pcb_ptr    The PCB pointer.
+ * @return error_t code
+ */
+error_t free_pcb(struct pcb_struct * pcb_ptr)
+{
+  if (pcb_ptr == NULL )
+    return E_NOT_FOUND;
+
+  if (sys_free_mem(pcb_ptr->stack_top) == -1)
+    return E_FREEMEM;
+  if (sys_free_mem(pcb_ptr->stack_base) == -1)
+    return E_FREEMEM;
+
+  struct pcb_queue_node * curr = pcb_ptr->other_pcb->head;
+
+  while ( curr != NULL )
+  {
+    curr = curr->next;
+
+    if (sys_free_mem(curr->prev) == -1)
+      return E_FREEMEM;
+  }
+
+  if (remove_pcb(pcb_ptr) != E_NOERROR)
+    return E_REMOVE_PCB;
+
+  if (sys_free_mem(pcb_ptr) == -1)
+    return E_FREEMEM;
+
+  return E_NOERROR;
+}
+
+/**
+ * @name pcb_struct
+ * @brief Will search all queues for a process named pName
+ *
+ * @param pName  The char pointer to the desired searched name
+ * @return PCB pointer if found, NULL if PCB is not found
+ */
+struct pcb_struct * find_pcb(const char * pName)
+{
+  struct pcb_queue_node * curr = ready_queue.head;
+
+  while ( curr != NULL )
+  {
+    if (strcmp((curr->actual_pcb).name, pName))
+      return &curr->actual_pcb;
+
+    curr = curr->next;
+  }
+
+  curr = blocked_queue.head;
+
+  while ( curr != NULL )
+  {
+    if (strcmp((curr->actual_pcb).name, pName))
+      return &curr->actual_pcb;
+
+    curr = curr->next;
+  }
+
+  return NULL;
 }
