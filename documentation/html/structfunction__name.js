2bb56accb6d97a601ddeac54f3d657caf32fc52e
Jenn <jqnguyen@mix.wvu.edu>
2016-02-26 03:32:23 +0000

    reran doxygen

--- documentation/html/structfunction__name.js
+++ documentation/html/structfunction__name.js
@@ -1,6 +1,6 @@
 var structfunction__name =
 [
-    [ "function", "structfunction__name.html#ad80214b3eea6c438c13ff5461c5350ab", null ],
+    [ "function", "structfunction__name.html#abed0ef4657db1edfdb54f82245f5681c", null ],
     [ "help", "structfunction__name.html#ac0f73e570d7d03a9f378a70e6d4d5632", null ],
     [ "nameStr", "structfunction__name.html#a7a94f7f31542a15b63160b6b213e0bcb", null ],
     [ "usage", "structfunction__name.html#a30f593e52febda0cc9d9703b9015fb0f", null ]
