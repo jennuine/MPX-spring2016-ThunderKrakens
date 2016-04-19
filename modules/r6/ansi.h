9a716e76254050bc57b07f55a1a43ca855c94266
Haofan Zheng <zhenghaven@users.noreply.github.com>
2016-04-19 00:42:59 -0400

    Improved ansi.h for other OS

--- modules/r6/ansi.h
+++ modules/r6/ansi.h
@@ -1,24 +1,54 @@
 
-// Text
-#define T_RESET "\x1B[0m" //reset all attrs
+#if defined(__linux__) || defined(linux) || defined(__linux)
 
-#define T_BOLD "\x1B[1m" //bold
-#define T_BOLD_OFF "\x1B[22m" //bold off
-#define T_ITCS "\x1B[3m" //italics
-#define T_ITCS_OFF "\x1B[23m" //italics off
+// Text
+#define T_RESET     "\x1B[0m" //reset all attrs
+
+#define T_BOLD      "\x1B[1m" //bold
+#define T_BOLD_OFF  "\x1B[22m" //bold off
+#define T_ITCS      "\x1B[3m" //italics
+#define T_ITCS_OFF  "\x1B[23m" //italics off
 
 
 // Text Colors
-#define T_NRM  "\x1B[39m" //normal/default
-#define T_RED  "\x1B[31m" //red
-#define T_CYAN  "\x1B[36m" //cyan
-#define T_WHT  "\x1B[37m" //white
+#define T_NRM       "\x1B[39m" //normal/default
+#define T_RED       "\x1B[31m" //red
+#define T_CYAN      "\x1B[36m" //cyan
+#define T_WHT       "\x1B[37m" //white
 
 // Background Colors
-#define B_NRM "\x1B[49m" //normal/default
-#define B_CYAN "\x1b[46m" //cyan
+#define B_NRM       "\x1B[49m" //normal/default
+#define B_CYAN      "\x1b[46m" //cyan
 
 
 
-#define T_DIR "\x1B[1m\x1B[36m" //color for directory
-#define T_DIR_OFF "\x1B[39m\x1B[22m"
+#define T_DIR       "\x1B[1m\x1B[36m" //color for directory
+#define T_DIR_OFF   "\x1B[39m\x1B[22m"
+
+#else
+	
+	// Text
+#define T_RESET     "" //reset all attrs
+
+#define T_BOLD      "" //bold
+#define T_BOLD_OFF  "" //bold off
+#define T_ITCS      "" //italics
+#define T_ITCS_OFF  "" //italics off
+
+
+	// Text Colors
+#define T_NRM       "" //normal/default
+#define T_RED       "" //red
+#define T_CYAN      "" //cyan
+#define T_WHT       "" //white
+
+	// Background Colors
+#define B_NRM       "" //normal/default
+#define B_CYAN      "" //cyan
+
+
+
+#define T_DIR       "" //color for directory
+#define T_DIR_OFF   ""
+
+#endif
