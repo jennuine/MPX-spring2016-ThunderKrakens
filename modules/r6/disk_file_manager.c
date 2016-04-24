72ef44b6564bcdbb14f0b3a07d1e1cd8b6939051
Jenn <jqnguyen@mix.wvu.edu>
2016-04-24 23:56:25 +0000

    edited makefile for r6 to automatically compile and execute img by number

--- modules/r6/disk_file_manager.c
+++ modules/r6/disk_file_manager.c
@@ -25,7 +25,6 @@ void type_file(struct dir_entry_info * file_entry_ptr)
     // printf("**DEBUG: file size is %d \n", file_entry_ptr->file_size);
     
     // struct file_itr * iter = fitr_init_offset(file_entry_ptr->first_log_clu, 4);
-
     
     for (fitr_begin(iter); !fitr_end(iter) && line_no > 0; fitr_next(iter))
     {
